d65df367da4655a2cda4f1fdd74ffc853cbc1439
henstyle <henspicsaypro@gmail.com>
2025-06-18 01:18:01 +0700

    Update login.bat

--- login.bat
+++ login.bat
@@ -1,8 +1,8 @@
 @echo off
 :: Buka Profile 1
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile1" --profile-directory="Default" --window-position=0,0
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile1" --profile-directory="Default" --window-position=0,0 --new-window "https://idx.google.com/joko"
 
 :: Buka Profile 2
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile2" --profile-directory="Default" --window-position=0,600
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile2" --profile-directory="Default" --window-position=0,600 --new-window "https://idx.google.com/joko"
 
 exit
