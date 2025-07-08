ad25d741f004a54bf50e40bbfebbfad5b2ee35d4
henstyle <henspicsaypro@gmail.com>
2025-07-08 14:19:18 +0700

    Update login.bat

--- login.bat
+++ login.bat
@@ -1,8 +1,21 @@
 @echo off
-:: Buka Profile 1
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile1" --profile-directory="Default" --window-position=0,0 --new-window "https://idx.google.com/joko"
 
-:: Buka Profile 2
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Administrator\Desktop\Profile2" --profile-directory="Default" --window-position=0,600 --new-window "https://idx.google.com/joko"
+:: Buka Profile 1 di posisi 0,0 dengan ukuran 500x500
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" ^
+  --user-data-dir="C:\Users\Administrator\Desktop\Profile1" ^
+  --profile-directory="Default" ^
+  --window-position=0,0 ^
+  --window-size=500,500 ^
+  --new-window "https://idx.google.com/joko"
+
+timeout /t 2 >nul
+
+:: Buka Profile 2 di posisi 0,500 (di bawahnya) dengan ukuran 500x500
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" ^
+  --user-data-dir="C:\Users\Administrator\Desktop\Profile2" ^
+  --profile-directory="Default" ^
+  --window-position=0,0 ^
+  --window-size=500,500 ^
+  --new-window "https://idx.google.com/joko"
 
 exit
